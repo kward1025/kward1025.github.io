a7ed9793e6e1a91598dcb828d87d594af54f9b6a
kward1025 <kward1025@gmail.com>
2019-06-28 11:48:02 -0700

    last day of class final changes

--- java.js
+++ java.js
@@ -2,3 +2,30 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.parallax');
     var instances = M.Parallax.init(elems);
   });
+
+  
+
+$(document).ready(function(){
+  $('.sidenav').sidenav();
+});
+
+$(function(){
+  $("form").submit(function(e){
+      e.preventDefault();
+      var href = $(this).attr("action");
+      $.ajax({
+          type: "POST",
+          dataType: "json",
+          url: href,
+          data: $(this).serialize(),
+          success: function(response){
+              if(response.status == "success"){
+        alert("We are in receipt of your submission!")
+        window.location.replace("https://kward1025.github.io/");
+              }else{
+                  alert("An error occured: " + response.message);
+              }
+          }
+      });
+  });
+});
