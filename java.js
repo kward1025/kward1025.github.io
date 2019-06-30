6a0b4b9b8000bf841fea0c8deb2a6f43b1f020f4
kward1025 <kward1025@gmail.com>
2019-06-29 19:58:40 -0700

    shariq's changes

--- java.js
+++ java.js
@@ -1,31 +1,48 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.parallax');
-    var instances = M.Parallax.init(elems);
-  });
-
-  
-
-$(document).ready(function(){
-  $('.sidenav').sidenav();
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.parallax');
+  var instances = M.Parallax.init(elems);
 });
 
-$(function(){
-  $("form").submit(function(e){
-      e.preventDefault();
-      var href = $(this).attr("action");
-      $.ajax({
-          type: "POST",
-          dataType: "json",
-          url: href,
-          data: $(this).serialize(),
-          success: function(response){
-              if(response.status == "success"){
-        alert("We are in receipt of your submission!")
-        window.location.replace("https://kward1025.github.io/");
-              }else{
-                  alert("An error occured: " + response.message);
-              }
-          }
-      });
+
+
+$(document).ready(function () {
+  $this = $('nav')
+
+  var $target = $($this.attr('data-target'));
+  $this.pushpin({
+    top: $target.offset().top,
+  });
+
+  $('.sidenav').sidenav();
+  $('.sidenav li a').click(function (e) {
+    $('.sidenav').sidenav('close');
+    return true;
+  });
+  $(".main-nav li a").click(function (e) {
+    // e.preventDefault();
+    var hash = e.currentTarget.hash;
+    $('.main-nav li').removeClass('active');
+    $(`.main-nav li a[href='${hash}']`).parent('li').addClass('active');
+  });
+});
+
+$(function () {
+  $("form").submit(function (e) {
+    e.preventDefault();
+    var href = $(this).attr("action");
+    $.ajax({
+      type: "POST",
+      dataType: "json",
+      url: href,
+      data: $(this).serialize(),
+      success: function (response) {
+        if (response.status == "success") {
+          alert("We are in receipt of your submission!")
+          window.location.replace("https://kward1025.github.io/");
+        } else {
+          alert("An error occured: " + response.message);
+        }
+      }
+    });
   });
 });
